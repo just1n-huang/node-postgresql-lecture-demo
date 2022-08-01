fc12f1729513faa0d33b28f8be193e3cfbfeedb7
Justin Huang <justin88@gmail.com>
2022-08-01 00:45:37 -0400

    data layer done

--- seed.sql
+++ seed.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS things;
+DROP TABLE IF EXISTS users;
 CREATE TABLE users(
     id SERIAL PRIMARY KEY, -- serial will auto increment, no input necessary
     name VARCHAR(100)
@@ -8,5 +8,17 @@ CREATE TABLE users(
 CREATE TABLE things(
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
-    description TEXT
+    description TEXT,
+    user_id INTEGER REFERENCES users(id)
 );
+
+
+INSERT INTO users(name) VALUES('moe');
+INSERT INTO users(name) VALUES('lucy');
+INSERT INTO users(name) VALUES('larry');
+INSERT INTO users(name) VALUES('ethyl');
+INSERT INTO users(name) VALUES('curly');
+
+INSERT INTO things(name, user_id) VALUES('foo', 1); 
+INSERT INTO things(name, user_id) VALUES('bar', 2); 
+INSERT INTO things(name, user_id) VALUES('baz', 3); 
